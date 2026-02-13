8fa4c733c54469727c50e835eedc9a56a1186a05
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-13 13:38:16 +0100

    MeteoSearchbar and MeteoHome state linked

--- meteo-app/src/components/MeteoHome.jsx
+++ meteo-app/src/components/MeteoHome.jsx
@@ -1,9 +1,11 @@
 import MeteoSearchbar from "./MeteoSearchbar";
+import { useState } from "react";
 
 const MeteoHome = function () {
+  const [currentWeatherURL, setCurrentWeatherURL] = useState(null);
   return (
     <main className="flex-grow-1">
-      <MeteoSearchbar></MeteoSearchbar>
+      <MeteoSearchbar setCurrentWeatherURL={setCurrentWeatherURL}></MeteoSearchbar>
     </main>
   );
 };
--- meteo-app/src/components/MeteoSearchbar.jsx
+++ meteo-app/src/components/MeteoSearchbar.jsx
@@ -4,20 +4,50 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
+import { useState } from "react";
+const apiKey = "8c213aae94d24bcb33da8a0f54e3d6e1";
 
-const MeteoSearchbar = function () {
+const MeteoSearchbar = function (props) {
+  const [cityName, setCityName] = useState("");
+  const [countryCode, setCountryCode] = useState("");
+
   return (
     <section>
       <Container fluid>
         <Alert className=" border-3">
           <h2 className="text-center text-secondary mb-3">Search the city you want to have info about</h2>
-          <Form>
+          <Form
+            onSubmit={(e) => {
+              e.preventDefault();
+              props.setCurrentWeatherURL(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=${apiKey}`);
+              setCityName("");
+              setCountryCode("");
+            }}
+          >
             <Row className="justify-content-center">
               <Col md={6} className="mb-3 mb-md-0">
-                <Form.Control type="text" placeholder="Type the name of the city here..." className=" mr-sm-2" />
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Type the name of the city here..."
+                  className=" mr-sm-2"
+                  value={cityName}
+                  onChange={(e) => {
+                    setCityName(e.target.value);
+                  }}
+                />
               </Col>
               <Col md={4} className="mb-3 mb-md-0">
-                <Form.Control type="text" placeholder="Type the country code here..." className=" mr-sm-2" />
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Type the country code here..."
+                  className=" mr-sm-2"
+                  value={countryCode}
+                  onChange={(e) => {
+                    setCountryCode(e.target.value);
+                  }}
+                />
               </Col>
               <Col md={2} className="d-flex flex-column">
                 <Button type="submit" className="bg-primary bg-opacity-75 border-0 fw-medium">
